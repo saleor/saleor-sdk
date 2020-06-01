8c8743273ba0967e26fb5a3548256a0c80ca105d
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-06-01 12:37:43 +0200

    Fix graphql types

--- src/mutations/gqlTypes/CreateUserAddress.ts
+++ src/mutations/gqlTypes/CreateUserAddress.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { AddressInput } from "./../../../gqlTypes/globalTypes";
+import { AddressInput } from "./../../gqlTypes/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: CreateUserAddress
--- src/mutations/gqlTypes/UpdateCheckoutBillingAddress.ts
+++ src/mutations/gqlTypes/UpdateCheckoutBillingAddress.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { AddressInput, CheckoutErrorCode } from "./../../../gqlTypes/globalTypes";
+import { AddressInput, CheckoutErrorCode } from "./../../gqlTypes/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: UpdateCheckoutBillingAddress
