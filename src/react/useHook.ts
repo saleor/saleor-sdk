d75a6cda3710f9a24dbc703e7479fd0c038885e4
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-07-09 15:58:03 +0200

    Update useHook

--- src/react/useHook.ts
+++ src/react/useHook.ts
@@ -1,15 +1,14 @@
-import React from "react";
 import { SaleorAPI } from "../api";
 import { useSaleorClient } from "./helpers";
 
 const useHook = <T extends keyof SaleorAPI>(dataName: T): SaleorAPI[T] => {
   const saleor = useSaleorClient();
 
-  const hookData = React.useMemo(() => {
+  const getHookData = () => {
     return saleor[dataName];
-  }, [dataName]);
+  };
 
-  return hookData;
+  return getHookData();
 };
 
 export const hookFactory = <T extends keyof SaleorAPI>(query: T) => () =>
