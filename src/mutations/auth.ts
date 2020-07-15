bd705e025199fbd49a69f42169a7fd4da502aef9
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-07-15 13:28:51 +0200

    Fix failing login test

--- src/mutations/auth.ts
+++ src/mutations/auth.ts
@@ -1,10 +1,8 @@
 import gql from "graphql-tag";
 
-import { userFragment } from "../fragments/auth";
 import { accountErrorFragment } from "../fragments/errors";
 
 export const tokenAuthMutation = gql`
-  ${userFragment}
   ${accountErrorFragment}
   mutation TokenAuth($email: String!, $password: String!) {
     tokenCreate(email: $email, password: $password) {
