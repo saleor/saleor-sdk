73582c677ecdabe96ea5ae49dd4aad8e7ad875bd
dominik-zeglen <flesz3@o2.pl>
2020-08-05 19:19:26 +0200

    Add tests for next method

--- src/api/collections/collections.test.ts
+++ src/api/collections/collections.test.ts
@@ -11,8 +11,33 @@ describe("Collection object", () => {
     });
 
     expect(list.data).toBeUndefined();
+    expect(list.loading).toBe(true);
     await list.current;
 
     expect(list.data).toMatchSnapshot();
+    expect(list.loading).toBe(false);
+  });
+
+  it("can get new page", async () => {
+    const collectionsAPI = new CollectionsAPI(client);
+    const list = collectionsAPI.getList({
+      count: 1,
+    });
+
+    expect(list.data).toBeUndefined();
+    expect(list.loading).toBe(true);
+    await list.current;
+
+    expect(list.data).toMatchSnapshot();
+    expect(list.loading).toBe(false);
+
+    list.next();
+
+    expect(list.loading).toBe(true);
+
+    await list.current;
+
+    expect(list.data).toMatchSnapshot();
+    expect(list.loading).toBe(false);
   });
 });
