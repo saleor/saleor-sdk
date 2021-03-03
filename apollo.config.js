2d8bb563974d158a1d90017df9bed6305bb14096
Piotr Grundas <piotrgrundas@gmail.com>
2021-03-03 23:28:17 +0100

    Fix tests

--- apollo.config.js
+++ apollo.config.js
@@ -7,8 +7,7 @@ module.exports = {
     ],
     service: {
       name: "saleor",
-      // url: "http://localhost:8000/graphql/",
-      url: "https://master.staging.saleor.cloud/graphql/",
+      url: "http://localhost:8000/graphql/",
     },
   },
 };
