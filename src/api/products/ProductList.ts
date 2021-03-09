f7833a939cd2e20ed7b150b36f37fa5c722104ae
Piotr Grundas <piotrgrundas@gmail.com>
2021-03-09 23:12:21 +0100

    Review fixes

--- src/api/products/ProductList.ts
+++ src/api/products/ProductList.ts
@@ -1,4 +1,3 @@
-// import { ApolloQueryResult } from "apollo-client";
 import BaseList, {
   GetPageInfo,
   GetTotalCount,
@@ -27,30 +26,9 @@ export class ProductList extends BaseList<
     ProductList_products_edges_node
   > = data => data.products?.edges.map(({ node }) => node);
 
-  query = async (variables: ProductListVariables) =>
+  query = (variables: ProductListVariables) =>
     this.client!.query<ProductListQuery, ProductListVariables>({
       query: productList,
       variables,
     });
-
-  // query = async (variables: ProductListVariables) =>
-  //   new Promise<ApolloQueryResult<ProductListQuery>>((resolve, reject) => {
-  //     const observableQuery = this.client!.watchQuery<
-  //       ProductListQuery,
-  //       ProductListVariables
-  //     >({
-  //       query: productList,
-  //       variables,
-  //     });
-
-  //     observableQuery.subscribe({
-  //       error: e => reject(e),
-  //       next: result => {
-  //         console.log(result);
-
-  //         resolve(result);
-  //       },
-  //     });
-  //     // .unsubscribe();
-  //   });
 }
