466cefa79b62d062d435abe8d235bac5a868fef7
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2021-07-09 11:25:02 +0200

    Update queries with new channel API

--- src/queries/attributes.ts
+++ src/queries/attributes.ts
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 
 export const attributes = gql`
   query Attributes($id: ID!, $channel: String) {
-    attributes(filter: { inCategory: $id, channel: $channel }, first: 100) {
+    attributes(channel: $channel, filter: { inCategory: $id }, first: 100) {
       edges {
         node {
           id
