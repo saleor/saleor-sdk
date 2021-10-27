aa37211699cb9761dd373a75e8a5ef3a6820c471
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2021-10-27 16:28:53 +0200

    Remove legacy test artifacts

--- test/mocks/externalObtainAccessTokens.ts
+++ test/mocks/externalObtainAccessTokens.ts
@@ -16,7 +16,6 @@ const externalObtainAccessTokens = (tokenExpirationPeriodInSeconds?: number) =>
     externalObtainAccessTokens: {
       __typename: "ExternalObtainAccessTokens",
       token: createTestToken(tokenExpirationPeriodInSeconds),
-      // "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzQ2MzYzMDAsImlzcyI6Im1hc3Rlci5zdGFnaW5nLnNhbGVvci5jbG91ZCIsIm93bmVyIjoic2FsZW9yIiwiZXhwIjoxNjM0NjM2NjAwLCJ0b2tlbiI6Ik0yaXJGbXpWQVNSMyIsImVtYWlsIjoidGVzdGVycytkYXNoYm9hcmRAc2FsZW9yLmlvIiwidHlwZSI6ImFjY2VzcyIsInVzZXJfaWQiOiJWWE5sY2pveE1ETXoiLCJpc19zdGFmZiI6dHJ1ZX0.XDO2-_pISP5ouclcchu-6fNeBnTbyYvZ86UIJBOM0lw",
       csrfToken: testCsrfToken,
       user: {
         id: "VXNlcjoxMDMz",
