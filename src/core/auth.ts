52e7b2dd8135d1a7c6cacc82a6fb04d82d3c7019
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2021-11-12 13:56:34 +0100

    Update API methods descriptions

--- src/core/auth.ts
+++ src/core/auth.ts
@@ -73,25 +73,107 @@ import { storage } from "./storage";
 import { USER } from "../apollo/queries";
 
 export interface AuthSDK {
+  /**
+   * Change the password of the logged in user.
+   *
+   * @param opts - Object with password and new password.
+   * @returns Errors if the passoword change has failed.
+   */
   changePassword: (opts: ChangePasswordOpts) => Promise<ChangePasswordResult>;
+  /**
+   * Authenticates user with email and password.
+   *
+   * @param opts - Object with user's email and password.
+   * @returns Promise resolved with CreateToken type data.
+   */
   login: (opts: LoginOpts) => Promise<LoginResult>;
+  /**
+   * Clears stored token and Apollo store. If external plugin was used to log in, the mutation will prepare
+   * the logout URL. All values passed in field input will be added as GET parameters to the logout request.
+   *
+   * @param opts - Object with input as JSON with returnTo - the URL where a user should be redirected
+   * when external plugin was used to log in
+   * @returns Logout data and errors if external plugin was used to log in. Otherwise null.
+   */
   logout: (opts?: LogoutOpts) => Promise<LogoutResult>;
+  /**
+   * Refresh JWT token. Mutation will try to take refreshToken from the function's arguments.
+   * If it fails, it will try to use refreshToken from the http-only cookie called refreshToken.
+   *
+   * @param includeUser - Whether to fetch user. Default false.
+   * @returns Authorization token.
+   */
   refreshToken: (includeUser?: boolean) => Promise<RefreshTokenResult>;
+  /**
+   * Registers user with email and password.
+   *
+   * @param opts - Object with user's data. Email and password are required fields.
+   * "channel" can be changed by using first "setChannel" method from api.
+   * @returns Promise resolved with AccountRegister type data.
+   */
   register: (opts: RegisterOpts) => Promise<RegisterResult>;
+  /**
+   * Sends an email with the account password modification link.
+   *
+   * @param opts - Object with slug of a channel which will be used for notify user,
+   * email of the user that will be used for password recovery and URL of a view
+   * where users should be redirected to reset the password. URL in RFC 1808 format.
+   *
+   * @returns Errors if there were some.
+   */
   requestPasswordReset: (
     opts: RequestPasswordResetOpts
   ) => Promise<RequestPasswordResetResult>;
+  /**
+   * Sets the user's password from the token sent by email.
+   *
+   * @param opts - Object with user's email, password and one-time token required to set the password.
+   * @returns User instance, JWT token, JWT refresh token and CSRF token.
+   */
   setPassword: (opts: SetPasswordOpts) => Promise<SetPasswordResult>;
+  /**
+   * Verify JWT token.
+   *
+   * @param token - Token value.
+   * @returns User assigned to token and the information if the token is valid or not.
+   */
   verifyToken: () => Promise<VerifyTokenResult>;
+  /**
+   * Executing externalAuthenticationUrl mutation will prepare special URL which will redirect user to requested
+   * page after successfull authentication. After redirection state and code fields will be added to the URL.
+   *
+   * @param opts - Object withpluginId default value set as "mirumee.authentication.openidconnect" and input as
+   * JSON with redirectUrl - the URL where the user should be redirected after successful authentication.
+   * @returns Authentication data and errors
+   */
   getExternalAuthUrl: (
     opts: GetExternalAuthUrlOpts
   ) => Promise<GetExternalAuthUrlResult>;
+  /**
+   * The externalObtainAccessTokens mutation will generate requested access tokens.
+   *
+   * @param opts - Object withpluginId default value set as "mirumee.authentication.openidconnect" and input as
+   * JSON with code - the authorization code received from the OAuth provider and state - the state value received
+   * from the OAuth provider
+   * @returns Login authentication data and errors
+   */
   getExternalAccessToken: (
     opts: GetExternalAccessTokenOpts
   ) => Promise<GetExternalAccessTokenResult>;
+  /**
+   * The externalRefresh mutation will generate new access tokens when provided with a valid refresh token.
+   *
+   * @param includeUser - Whether to fetch user. Default false.
+   * @returns Token refresh data and errors
+   */
   refreshExternalToken: (
     includeUser?: boolean
   ) => Promise<RefreshExternalTokenResult>;
+  /**
+   * The mutation will verify the authentication token.
+   *
+   * @returns Token verification data and errors
+   */
   verifyExternalToken: () => Promise<VerifyExternalTokenResult>;
 }
 
@@ -99,12 +181,6 @@ export const auth = ({
   apolloClient: client,
   channel,
 }: SaleorClientMethodsProps): AuthSDK => {
-  /**
-   * Authenticates user with email and password.
-   *
-   * @param opts - Object with user's email and password.
-   * @returns Promise resolved with CreateToken type data.
-   */
   const login: AuthSDK["login"] = opts => {
     client.writeQuery({
       query: USER,
@@ -129,14 +205,6 @@ export const auth = ({
     });
   };
 
-  /**
-   * Clears stored token and Apollo store. If external plugin was used to log in, the mutation will prepare
-   * the logout URL. All values passed in field input will be added as GET parameters to the logout request.
-   *
-   * @param opts - Object with input as JSON with returnTo - the URL where a user should be redirected
-   * when external plugin was used to log in
-   * @returns Logout data and errors if external plugin was used to log in. Otherwise null.
-   */
   const logout: AuthSDK["logout"] = async opts => {
     const authPluginId = storage.getAuthPluginId();
 
@@ -173,13 +241,6 @@ export const auth = ({
     return null;
   };
 
-  /**
-   * Registers user with email and password.
-   *
-   * @param opts - Object with user's data. Email and password are required fields.
-   * "channel" can be changed by using first "setChannel" method from api.
-   * @returns Promise resolved with AccountRegister type data.
-   */
   const register: AuthSDK["register"] = async opts =>
     await client.mutate<RegisterMutation, RegisterMutationVariables>({
       mutation: REGISTER,
@@ -191,13 +252,6 @@ export const auth = ({
       },
     });
 
-  /**
-   * Refresh JWT token. Mutation will try to take refreshToken from the function's arguments.
-   * If it fails, it will try to use refreshToken from the http-only cookie called refreshToken.
-   *
-   * @param includeUser - Whether to fetch user. Default false.
-   * @returns Authorization token.
-   */
   const refreshToken: AuthSDK["refreshToken"] = (includeUser = false) => {
     const csrfToken = storage.getCSRFToken();
 
@@ -239,12 +293,6 @@ export const auth = ({
     });
   };
 
-  /**
-   * Verify JWT token.
-   *
-   * @param token - Token value.
-   * @returns User assigned to token and the information if the token is valid or not.
-   */
   const verifyToken: AuthSDK["verifyToken"] = async () => {
     const token = storage.getAccessToken();
 
@@ -267,12 +315,6 @@ export const auth = ({
     return result;
   };
 
-  /**
-   * Change the password of the logged in user.
-   *
-   * @param opts - Object with password and new password.
-   * @returns Errors if the passoword change has failed.
-   */
   const changePassword: AuthSDK["changePassword"] = async opts => {
     const result = await client.mutate<
       PasswordChangeMutation,
@@ -285,15 +327,6 @@ export const auth = ({
     return result;
   };
 
-  /**
-   * Sends an email with the account password modification link.
-   *
-   * @param opts - Object with slug of a channel which will be used for notify user,
-   * email of the user that will be used for password recovery and URL of a view
-   * where users should be redirected to reset the password. URL in RFC 1808 format.
-   *
-   * @returns Errors if there were some.
-   */
   const requestPasswordReset: AuthSDK["requestPasswordReset"] = async opts => {
     const result = await client.mutate<
       RequestPasswordResetMutation,
@@ -306,12 +339,6 @@ export const auth = ({
     return result;
   };
 
-  /**
-   * Sets the user's password from the token sent by email.
-   *
-   * @param opts - Object with user's email, password and one-time token required to set the password.
-   * @returns User instance, JWT token, JWT refresh token and CSRF token.
-   */
   const setPassword: AuthSDK["setPassword"] = opts => {
     return client.mutate<SetPasswordMutation, SetPasswordMutationVariables>({
       mutation: SET_PASSWORD,
@@ -327,14 +354,6 @@ export const auth = ({
     });
   };
 
-  /**
-   * Executing externalAuthenticationUrl mutation will prepare special URL which will redirect user to requested
-   * page after successfull authentication. After redirection state and code fields will be added to the URL.
-   *
-   * @param opts - Object withpluginId default value set as "mirumee.authentication.openidconnect" and input as
-   * JSON with redirectUrl - the URL where the user should be redirected after successful authentication.
-   * @returns Authentication data and errors
-   */
   const getExternalAuthUrl: AuthSDK["getExternalAuthUrl"] = async opts => {
     const result = await client.mutate<
       ExternalAuthenticationUrlMutation,
@@ -347,14 +366,6 @@ export const auth = ({
     return result;
   };
 
-  /**
-   * The externalObtainAccessTokens mutation will generate requested access tokens.
-   *
-   * @param opts - Object withpluginId default value set as "mirumee.authentication.openidconnect" and input as
-   * JSON with code - the authorization code received from the OAuth provider and state - the state value received
-   * from the OAuth provider
-   * @returns Login authentication data and errors
-   */
   const getExternalAccessToken: AuthSDK["getExternalAccessToken"] = opts => {
     client.writeQuery({
       query: USER,
@@ -383,12 +394,6 @@ export const auth = ({
     });
   };
 
-  /**
-   * The externalRefresh mutation will generate new access tokens when provided with a valid refresh token.
-   *
-   * @param includeUser - Whether to fetch user. Default false.
-   * @returns Token refresh data and errors
-   */
   const refreshExternalToken: AuthSDK["refreshExternalToken"] = (
     includeUser = false
   ) => {
@@ -448,11 +453,6 @@ export const auth = ({
     });
   };
 
-  /**
-   * The mutation will verify the authentication token.
-   *
-   * @returns Token verification data and errors
-   */
   const verifyExternalToken: AuthSDK["verifyExternalToken"] = async () => {
     const csrfToken = storage.getCSRFToken();
     const authPluginId = storage.getAuthPluginId();
--- src/core/user.ts
+++ src/core/user.ts
@@ -56,27 +56,88 @@ import {
 } from "./types";
 
 export interface UserSDK {
+  /**
+   * Remove user account.
+   *
+   * @param token - A one-time token required to remove account. Sent by email using AccountRequestDeletion mutation.
+   * @returns Deleted user's account data and errors.
+   */
   accountDelete: (token: string) => Promise<AccountDeleteResult>;
+  /**
+   * Sends an email with the account removal link for the logged-in user.
+   *
+   * @param redirectUrl - URL of a view where users should be redirected to delete their account. URL in RFC 1808 format.
+   * @returns Errors if there are some.
+   */
   accountRequestDeletion: (
     redirectUrl: string
   ) => Promise<AccountRequestDeletionResult>;
+  /**
+   * Confirm the email change of the logged-in user.
+   *
+   * @param token - A one-time token required to change the email.
+   * @returns A user instance with a new email and errors.
+   */
   confirmEmailChange: (token: string) => Promise<ConfirmEmailChangeResult>;
+  /**
+   * Create a new address for the account.
+   *
+   * @param opts - Object with fields required to create address and a type of address.
+   * If provided, the new address will be automatically assigned as the customer's default address of that type.
+   * @returns Updated user account.
+   */
   createAccountAddress: (
     opts: CreateAccountAddressOpts
   ) => Promise<CreateAccountAddressResult>;
+  /**
+   * Delete an address of the logged-in account.
+   *
+   * @param addressId - ID of the address to delete.
+   * @returns Updated user account.
+   */
   deleteAccountAddress: (
     addressId: string
   ) => Promise<DeleteAccountAddressResult>;
+  /**
+   * Request email change of the logged in user.
+   *
+   * @param opts - Object with new user email, user's password and URL of a view where users should be redirected to update the email address.
+   * @returns A user instance and errors.
+   */
   requestEmailChange: (
     opts: RequestEmailChangeOpts
   ) => Promise<RequestEmailChangeResult>;
+  /**
+   * Sets a default address for the authenticated account.
+   *
+   * @param opts - Object with ID of the address to set as default and the type of address.
+   * @returns Updated user account.
+   */
   setAccountDefaultAddress: (
     opts: SetAccountDefaultAddressOpts
   ) => Promise<SetAccountDefaultAddressResult>;
+  /**
+   * Updates the account of the logged-in account.
+   *
+   * @param opts - Fields required to update the account of the logged-in account.
+   * @returns Updated user account.
+   */
   updateAccount: (opts: UpdateAccountOpts) => Promise<UpdateAccountResult>;
+  /**
+   * Updates an address of the logged-in account.
+   *
+   * @param opts - Object with ID of the address to update and fields required to update the address.
+   * @returns Updated user account.
+   */
   updateAccountAddress: (
     opts: UpdateAccountAddressOpts
   ) => Promise<UpdateAccountAddressResult>;
+  /**
+   * Confirms user account with token sent by email during registration.
+   *
+   * @param opts - Object with email of the user and one-time token required to confirm the account.
+   * @returns Confirmed user account.
+   */
   confirmAccount: (opts: ConfirmAccountOpts) => Promise<ConfirmAccountResult>;
 }
 
@@ -86,12 +147,6 @@ export const user = ({
 }: SaleorClientMethodsProps): UserSDK => {
   const _auth = auth({ apolloClient: client, channel });
 
-  /**
-   * Remove user account.
-   *
-   * @param token - A one-time token required to remove account. Sent by email using AccountRequestDeletion mutation.
-   * @returns Deleted user's account data and errors.
-   */
   const accountDelete: UserSDK["accountDelete"] = async token => {
     const result = await client.mutate<
       AccountDeleteMutation,
@@ -106,12 +161,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Sends an email with the account removal link for the logged-in user.
-   *
-   * @param redirectUrl - URL of a view where users should be redirected to delete their account. URL in RFC 1808 format.
-   * @returns Errors if there are some.
-   */
   const accountRequestDeletion: UserSDK["accountRequestDeletion"] = async redirectUrl => {
     const result = await client.mutate<
       AccountRequestDeletionMutation,
@@ -124,12 +173,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Confirm the email change of the logged-in user.
-   *
-   * @param token - A one-time token required to change the email.
-   * @returns A user instance with a new email and errors.
-   */
   const confirmEmailChange: UserSDK["confirmEmailChange"] = async token => {
     const result = await client.mutate<
       ConfirmEmailChangeMutation,
@@ -142,12 +185,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Request email change of the logged in user.
-   *
-   * @param opts - Object with new user email, user's password and URL of a view where users should be redirected to update the email address.
-   * @returns A user instance and errors.
-   */
   const requestEmailChange: UserSDK["requestEmailChange"] = async opts => {
     const result = await client.mutate<
       RequestEmailChangeMutation,
@@ -160,12 +197,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Updates the account of the logged-in account.
-   *
-   * @param opts - Fields required to update the account of the logged-in account.
-   * @returns Updated user account.
-   */
   const updateAccount: UserSDK["updateAccount"] = async opts => {
     const result = await client.mutate<
       AccountUpdateMutation,
@@ -178,12 +209,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Sets a default address for the authenticated account.
-   *
-   * @param opts - Object with ID of the address to set as default and the type of address.
-   * @returns Updated user account.
-   */
   const setAccountDefaultAddress: UserSDK["setAccountDefaultAddress"] = async opts => {
     const result = await client.mutate<
       SetAccountDefaultAddressMutation,
@@ -196,13 +221,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Create a new address for the account.
-   *
-   * @param opts - Object with fields required to create address and a type of address.
-   * If provided, the new address will be automatically assigned as the customer's default address of that type.
-   * @returns Updated user account.
-   */
   const createAccountAddress: UserSDK["createAccountAddress"] = async opts => {
     const result = await client.mutate<
       CreateAccountAddressMutation,
@@ -215,12 +233,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Delete an address of the logged-in account.
-   *
-   * @param addressId - ID of the address to delete.
-   * @returns Updated user account.
-   */
   const deleteAccountAddress: UserSDK["deleteAccountAddress"] = async addressId => {
     const result = await client.mutate<
       DeleteAccountAddressMutation,
@@ -233,12 +245,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Updates an address of the logged-in account.
-   *
-   * @param opts - Object with ID of the address to update and fields required to update the address.
-   * @returns Updated user account.
-   */
   const updateAccountAddress: UserSDK["updateAccountAddress"] = async opts => {
     const result = await client.mutate<
       UpdateAccountAddressMutation,
@@ -251,12 +257,6 @@ export const user = ({
     return result;
   };
 
-  /**
-   * Confirms user account with token sent by email during registration.
-   *
-   * @param opts - Object with email of the user and one-time token required to confirm the account.
-   * @returns Confirmed user account.
-   */
   const confirmAccount: UserSDK["confirmAccount"] = async opts => {
     const result = await client.mutate<
       AccountConfirmMutation,
